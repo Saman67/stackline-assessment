755458a183532ca7bc7b12393fc08c9de173087e
Saman <saman.missaghian@gmail.com>
2024-06-29 22:05:06 -0700

    Add github page deployment

--- src/service/product/index.ts
+++ src/service/product/index.ts
@@ -1,5 +1,5 @@
 
 export async function getProduct() {
-  const response = await fetch('/data/stackline_frontend_assessment_data_2021.json');
+  const response = await fetch('https://saman67.github.io/stackline-assessment/data/stackline_frontend_assessment_data_2021.json');
   return await response.json();
 }
